87acd58ef95b3553c9f7875fb8a39cbef8dc35b3
johnlee0510 <johnlee0510@gmail.com>
2016-05-02 17:25:12 -0400

    Added files via upload

--- MainMenu.java
+++ MainMenu.java
@@ -1,11 +1,15 @@
 package project;
-import java.awt.*;
 
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
 
 import javax.swing.*;
 
 public class MainMenu extends JFrame {
-private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 	private JFrame main;
 	private JPanel mainsource;
 	private JLabel title_cus = new JLabel("Welcome Customer!");
@@ -15,9 +19,10 @@ private static final long serialVersionUID = 1L;
 	private JButton listPlane = new JButton("List Planes");
 	private JButton userFlight = new JButton("Users Flight");
 	private JButton bookFlight = new JButton("Add/Update Flight");
+	private JButton editUserFlight = new JButton("Edit Users Flight");
 	private JButton logout = new JButton("Logout");
 	private JButton userprofile = new JButton("Users profile");
-
+	public String username;
 	public void customerRun() {
 		main = new JFrame();
 		mainsource = new JPanel();
@@ -34,15 +39,31 @@ private static final long serialVersionUID = 1L;
 
 		listFlight.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent ex) {
-				main.dispose();
 				new flight_info();
 			}
 		});
-
+		myFlight.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ex) {
+				try {
+					new Myflight(username);
+				} catch (SQLException e) {
+					//if something went wrong with SQL connection give error
+					e.printStackTrace();
+				}
+			}
+		});
+		bookFlight.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ex) {
+				new reservation();
+			}
+		});
+		
 		logout.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent ex) {
 				JOptionPane.showMessageDialog(null, "logged out- Good bye");
-				System.exit(0);
+				main.dispose();
+				new login();
+				
 
 			}
 		});
@@ -57,6 +78,7 @@ private static final long serialVersionUID = 1L;
 		mainsource.add(listPlane);
 		mainsource.add(listFlight);
 		mainsource.add(userFlight);
+		mainsource.add(editUserFlight);
 		mainsource.add(userprofile);
 		mainsource.add(logout);
 		mainsource.setVisible(true);
@@ -64,28 +86,70 @@ private static final long serialVersionUID = 1L;
 		main.add(mainsource);
 		main.setVisible(true);
 
+		userFlight.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ex) {
+				try {
+					new User_flight();
+				} catch (SQLException e) {
+					//if something went wrong with SQL connection give error
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		userprofile.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ex) {
+				try {
+					new User_profiles();
+				} catch (SQLException e) {
+					//if something went wrong with SQL connection give error
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		listPlane.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ex) {
+				try {
+					new plane();
+				} catch (SQLException e) {
+					//if something went wrong with SQL connection give error
+					e.printStackTrace();
+				}
+			}
+		});
+		
 		listFlight.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent ex) {
-				main.dispose();
 				new flight_info();
 			}
 		});
+		
+		editUserFlight.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ex) {
+				new reservation();
+			}
+		});
+		
+		
 		logout.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent ex) {
 				JOptionPane.showMessageDialog(null, "logged out- Good bye");
-				System.exit(0);
+				main.dispose();
+				new login();
 
 			}
 		});
 
 	}
 
-	public MainMenu(char a) {
+	public MainMenu(String a) {
 		System.out.println("hello user");
 		customerRun();
 	}
 
-	public MainMenu(String a) {
+
+	public MainMenu(String rs_username, char a) {
 		System.out.println("hello administrator");
 		adminRun();
 	}
